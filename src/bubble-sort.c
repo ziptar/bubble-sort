0f0a9d3cd072ba2ce802e033996775512837cb09
said <said@skynet.skynet.local>
2020-06-29 07:11:10 +0100

    Initial commit

---
+++ src/bubble-sort.c
@@ -0,0 +1,37 @@
+/*
+ ============================================================================
+ Name        : bubble-sort.c
+ Author      : Said TALEB ALI
+ Version     : 1.0
+ Copyright   : MIT
+ Description : Sort an array using bubble sort.
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define LENGTH 10
+
+int main(void) {
+
+	int list[LENGTH] = {12 ,-5 ,0 ,13 ,5 ,8 ,3 ,-4 ,-1 ,20};
+
+	for (int i = 0; i < LENGTH - 1; ++i) {
+		for (int j = 0, temp = 0; j < LENGTH - 1 - i; ++j) {
+			if (list[j] > list[j + 1])
+			{
+				temp = list[j];
+				list[j] = list[j + 1];
+				list[j + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < LENGTH; ++i) {
+		printf("%d ", list[i]);
+	}
+
+	return EXIT_SUCCESS;
+}
