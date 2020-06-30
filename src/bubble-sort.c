e85bc1e241620bceefbabfd9d65515a106082cc2
said <said@skynet.skynet.local>
2020-06-30 04:09:29 +0100

    Optimizing the bubble sort

--- src/bubble-sort.c
+++ src/bubble-sort.c
@@ -17,16 +17,21 @@
 int main(void) {
 
 	int list[LENGTH] = {12 ,-5 ,0 ,13 ,5 ,8 ,3 ,-4 ,-1 ,20};
+	bool swapped;
 
 	for (int i = 0; i < LENGTH - 1; ++i) {
+		swapped = false;
 		for (int j = 0, temp = 0; j < LENGTH - 1 - i; ++j) {
 			if (list[j] > list[j + 1])
 			{
 				temp = list[j];
 				list[j] = list[j + 1];
 				list[j + 1] = temp;
+				swapped = true;
 			}
 		}
+		// If no two elements were swapped by inner loop, then break
+		if (!swapped) break;
 	}
 
 	for (int i = 0; i < LENGTH; ++i) {
